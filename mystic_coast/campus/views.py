220aa06d914b0b23dd2e9af8b4ddc841aad49d88
wkatbey <lunarxparallax@gmail.com>
2019-03-05 18:10:36 -0500

    Creating default header template in index.html

--- mystic_coast/campus/views.py
+++ mystic_coast/campus/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 def index(request):
     context = {'default_data': ''}
     return render(request, 'campus/index.html', context)
+
+def restaurant_list(request):
+    conext = {'default_data': ''}
+    return render(request, 'campus/restaurant-list.html', context)
+
+    
