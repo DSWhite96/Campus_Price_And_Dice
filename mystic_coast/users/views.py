b8af1d729849fd3275edc0310c0710ab23e56eef
Derrick <gf6643@wayne.edu>
2019-04-20 23:04:25 -0400

    Added a login and logout page

--- mystic_coast/users/views.py
+++ mystic_coast/users/views.py
@@ -10,7 +10,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('campus:index')
+            return redirect('login')
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form':form})
